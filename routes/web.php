2c1dd7e6ec9b539c1c900acf4ed0dcc36a18922c
Megatron <rafaeldacunhatheodoro@gmail.com>
2023-07-17 16:21:21 -0300

    CRUD e readme em deseonvimento

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CadastroControllers;
 use Illuminate\Support\Facades\Route;
+//rota do bd
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cadastros', [CadastroControllers::class, 'index']);
+
+//Route db conection
+Route::resource('/products', ProductController::class);
